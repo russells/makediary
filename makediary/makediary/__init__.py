5a0f9ec1023b7648ec8f2a735fdcf1d05588e7cb
Russell Steicke <russells@adelie.cx>
2004-04-16 17:31:30 +0000

    Updated version in makediary/__init__.py.
    Added explanation in VERSION of all the places the version number appears.
    
    git-archimport-id: russells@adelie.cx--2004a/makediary--trunk--0--patch-50

--- makediary/makediary/__init__.py
+++ makediary/makediary/__init__.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python2
 
-# $Id: __init__.py 18 2002-12-23 05:57:39Z anonymous $
+# $Id: __init__.py 62 2003-01-05 11:54:16Z anonymous $
 
 """A package for printing book style diaries."""
 
 
-__version__ = '0.1'
+__version__ = '0.1.1'
 
 __all__ = ['makediary',
            'DotCalendar',
