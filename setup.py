f835f499610c7eff51d050c997913ee914ff7df4
Russell Steicke <russells@adelie.cx>
2010-01-14 11:22:50 +0800

    Hack around the python installation paths.
    On some later python versions (2.6 and onwards on Ubuntu) the paths
    used by packages has changed.  I was putting the makediary stuff into
    lib/site-python/, but that doesn't work on those versions.
    
    Change the distutils stuff so it works on those versions.
    
    This hack is not ideal, as it detects the python version and changes
    behaviour depending on that.  The whole of ./make-deb and ./debian/
    need to be reviewed and cleaned up.
    
    Signed-off-by: Russell Steicke <russells@adelie.cx>

--- setup.py
+++ setup.py
@@ -7,18 +7,6 @@ from distutils.core import setup
 
 _makeDiaryVersion = '0.2.96'
 
-from glob import glob
-
-
-def globEPSFiles(base):
-    files = []
-    for f in glob('eps/%s/%s*.tex' % (base, base)):
-        files.append(f)
-    for f in glob('eps/%s/%s*.eps' % (base, base)):
-        files.append(f)
-    return files
-
-
 setup(name='makediary',
       description="""Make printed diaries.""",
 
@@ -34,28 +22,8 @@ setup(name='makediary',
       version=_makeDiaryVersion,
       license="GPL",
       packages=['makediary'],
+      package_dir={'makediary' : 'makediary' },
+      package_data={'makediary': ['eps/*/*.tex', 'eps/*/*.eps'] },
       scripts=['bin/makediary'],
-      data_files=[('share/man/man1', ['man/makediary.1.gz']),
-                  ('lib/site-python/makediary/eps/vi',
-                   globEPSFiles('vi')
-                  ),
-                  ('lib/site-python/makediary/eps/unix',
-                   ['eps/unix/unix.tex',
-                    'eps/unix/unix.eps',
-		   ]
-		  ),
-                  ('lib/site-python/makediary/eps/sh',
-                   ['eps/sh/sh.tex',
-                    'eps/sh/sh.eps',
-                   ]
-                  ),
-                  ('lib/site-python/makediary/eps/units',
-                   ['eps/units/units.tex',
-                    'eps/units/units.eps',
-                   ]
-                  ),
-                  ('lib/site-python/makediary/eps/sed',
-                   globEPSFiles('sed')
-                  ),
-                 ])
+      data_files=[('share/man/man1', ['man/makediary.1.gz']) ])
 
