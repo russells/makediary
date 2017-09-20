83edad07add84024409ac25389e85ab88ef1dfcd
Russell Steicke <russells@adelie.cx>
2017-09-20 17:28:03 +0800

    Bump version to 0.3.1.
    Signed-off-by: Russell Steicke <russells@adelie.cx>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 
 # This version number must match the version number in VERSION.
 
-_makeDiaryVersion = '0.3'
+_makeDiaryVersion = '0.3.1'
 
 setup(name='makediary',
       description="""Make printed diaries.""",
