3e1c8c477e8013d0bbf1ed9ff2ade89dd0793f5f
Russell Steicke <russells@adelie.cx>
2025-12-12 14:47:24 +0800

    Test: improve the test suite.
    Signed-off-by: Russell Steicke <russells@adelie.cx>

--- test-all-diary-types.sh
+++ test-all-diary-types.sh
@@ -8,62 +8,78 @@ export PYTHONPATH=`pwd` #:`pwd`/makediary
 P=${P:-python3}
 MD=${MD:-./bin/makediary}
 
-set -x -e
+function expect_success () {
+	echo -n 'Expect success: `'"$@""' ... "
+	"$@"
+	local c=$?
+	echo done
+	[ 0 -ne "$c" ] && exit "$c"
+}
 
-$P $MD
-$P $MD --output=d.ps
-$P $MD --pdf
-$P $MD --pdf --output=d.pdf
+function expect_FAILURE () {
+	echo -n 'Expect FAILURE: `'"$@""' ... "
+	"$@"
+	local c=$?
+	[ 0 -eq "$c" ] && exit 99
+	echo done
+}
 
-$P $MD --layout=day-to-page
-$P $MD --layout=logbook
-$P $MD --layout=week-to-opening
-$P $MD --layout=week-to-2-openings
-$P $MD --layout=week-to-page
-$P $MD --layout=week-with-notes
-$P $MD --layout=work
+expect_success $P $MD
+expect_success $P $MD --output=d.ps
+expect_success $P $MD --pdf
+expect_success $P $MD --pdf --output=d.pdf
 
-$P $MD --address-pages=0
-$P $MD --address-pages=44
-$P $MD --calendar-pages=no
-$P $MD --calendar-pages=yes
-$P $MD --colour
-$P $MD --day-title-shading=all
-$P $MD --day-title-shading=holidays
-$P $MD --day-title-shading=none
-$P $MD --debug-boxes
-$P $MD --debug-version
-$P $MD --debug-whole-page-boxes
-$P $MD --expense-pages=0
-$P $MD --expense-pages=2
-$P $MD --expense-pages=4
-$P $MD --expense-pages=40
-$P $MD --gridded-notes
-$P $MD --large-planner
-$P $MD --line-colour=red
-$P $MD --line-thickness=1
-$P $MD --margins-multiplier=3
-$P $MD --northern-hemisphere-moon
-$P $MD --no-appointment-times
-$P $MD --no-smiley
-$P $MD --notes-pages=0
-$P $MD --notes-pages=100
-$P $MD --notes-pages=-7
-$P $MD --page-numbers
-$P $MD --page-registration-marks
-$P $MD --page-x-offset=7 --page-y-offset=18
-$P $MD --planner-years=16
-$P $MD --pcal
-$P $MD --pcal-planner
-$P $MD --perpetual-calendars
-$P $MD --personal-info-no-work
-$P $MD --shading=no
-$P $MD --shading=yes
-$P $MD --sed-ref --sh-ref
-$P $MD --unix-ref --vi-ref
-$P $MD --weeks-before=17 --weeks-after=31
-$P $MD --page-size=filofax --paper-size=a0
-$P $MD --page-size=letter --paper-size=a3
-$P $MD --page-size=a4 --paper-size=a3
-$P $MD --page-size=a5 --paper-size=a4
+expect_success $P $MD --layout=day-to-page
+expect_success $P $MD --layout=logbook
+expect_success $P $MD --layout=week-to-opening
+expect_success $P $MD --layout=week-to-2-openings
+expect_success $P $MD --layout=week-to-page
+expect_success $P $MD --layout=week-with-notes
+expect_success $P $MD --layout=work
+
+expect_success $P $MD --address-pages=0
+expect_success $P $MD --address-pages=44
+expect_success $P $MD --calendar-pages=no
+expect_success $P $MD --calendar-pages=yes
+expect_success $P $MD --colour
+expect_success $P $MD --day-title-shading=all
+expect_success $P $MD --day-title-shading=holidays
+expect_success $P $MD --day-title-shading=none
+expect_success $P $MD --debug-boxes
+expect_success $P $MD --debug-version
+expect_success $P $MD --debug-whole-page-boxes
+expect_success $P $MD --expense-pages=0
+expect_success $P $MD --expense-pages=2
+expect_success $P $MD --expense-pages=4
+expect_FAILURE $P $MD --expense-pages=40
+expect_success $P $MD --gridded-notes
+expect_success $P $MD --large-planner
+expect_success $P $MD --line-colour=red
+expect_success $P $MD --line-thickness=1
+expect_success $P $MD --margins-multiplier=3
+expect_success $P $MD --northern-hemisphere-moon
+expect_success $P $MD --no-appointment-times
+expect_success $P $MD --no-smiley
+expect_success $P $MD --notes-pages=0
+expect_success $P $MD --notes-pages=100
+expect_success $P $MD --notes-pages=-7
+expect_success $P $MD --page-numbers
+expect_success $P $MD --page-registration-marks
+expect_success $P $MD --page-x-offset=7 --page-y-offset=18
+expect_success $P $MD --planner-years=16
+expect_success $P $MD --pcal
+expect_success $P $MD --pcal-planner
+expect_success $P $MD --perpetual-calendars
+expect_success $P $MD --personal-info-no-work
+expect_success $P $MD --shading=no
+expect_success $P $MD --shading=yes
+expect_success $P $MD --sed-ref --sh-ref
+expect_success $P $MD --unix-ref --vi-ref
+expect_success $P $MD --man-page=ls
+expect_success $P $MD --man-page=exit,2 --man-page=exit,3 --man-page=bash
+expect_success $P $MD --weeks-before=17 --weeks-after=31
+expect_success $P $MD --page-size=filofax --paper-size=a0
+expect_success $P $MD --page-size=letter --paper-size=a3
+expect_success $P $MD --page-size=a4 --paper-size=a3
+expect_success $P $MD --page-size=a5 --paper-size=a4
 
